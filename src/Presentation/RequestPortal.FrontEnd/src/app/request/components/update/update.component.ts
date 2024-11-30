dd66b5957dbcb3226b338f605a6486b52daf1891
StasHlushakou <stas.hlushakou@gmail.com>
2024-11-30 18:43:42 +0300

    small update

--- src/Presentation/RequestPortal.FrontEnd/src/app/request/components/update/update.component.ts
+++ src/Presentation/RequestPortal.FrontEnd/src/app/request/components/update/update.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RequestService } from '../../../services/request.service';
+import { CreateRequestDTO, RequestDTO } from '../../types/requestDTO.interface';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'rrp-update-request',
@@ -7,15 +16,81 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./update.component.scss'],
 })
 export class UpdateRequestComponent implements OnInit {
-  id: string | null = '';
+  id!: string;
+  request!: RequestDTO;
 
-  constructor(private route: ActivatedRoute) {}
+  requestForm!: FormGroup;
+  isSomethingWrong = false;
+  isRequestProcessing = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private router: Router,
+    private requestService: RequestService
+  ) {}
 
   ngOnInit(): void {
     this.initializeValues();
   }
 
   initializeValues(): void {
-    this.id = this.route.snapshot.paramMap.get('slug');
+    this.id = this.route.snapshot.paramMap.get('id') ?? '';
+    this.loadData();
+  }
+
+  loadData(): void {
+    if (this.id) {
+      this.requestService
+        .getRequestById(this.id)
+        .pipe(first())
+        .subscribe(
+          (res: RequestDTO) => {
+            this.request = res;
+            this.initializeForm();
+          },
+          (err) => {
+            console.log('error');
+          }
+        );
+    }
+  }
+
+  initializeForm(): void {
+    this.requestForm = this.fb.group({
+      theme: new FormControl(this.request.theme, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20),
+      ]),
+      description: new FormControl(this.request.description, [
+        Validators.required,
+        Validators.maxLength(500),
+      ]),
+    });
+  }
+
+  onSubmit(): void {
+    const request: CreateRequestDTO = {
+      theme: this.requestForm.value.theme,
+      description: this.requestForm.value.description,
+    };
+    this.isRequestProcessing = true;
+    this.requestService
+      .updateRequest(this.id, request)
+      .pipe(first())
+      .subscribe(
+        () => {
+          this.router.navigate(['/']);
+        },
+        (err) => {
+          this.isRequestProcessing = false;
+          this.isSomethingWrong = true;
+        }
+      );
+  }
+
+  back(): void {
+    this.router.navigate(['']);
   }
 }
